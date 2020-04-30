bc3b6f309aa66258dde81d43d7b5d936903e6c3d
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-04-30 15:03:23 -0300

    define requirement to ensure api usage

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,83 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import * as api from './services/api';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Mercado Livre Api functions', () => {
+  it('getCategories', () => {
+    const successResponseBody = [{
+      "id": "MLB5672",
+      "name": "Acessórios para Veículos"
+    }, {
+      "id": "MLB271599",
+      "name": "Agro"
+    }, {
+      "id": "MLB1403",
+      "name": "Alimentos e Bebidas"
+    }];
+
+    const mockFetchPromise = Promise.resolve({
+      json: () => Promise.resolve(successResponseBody)
+    });
+
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    return api.getCategories().then(categories => {
+      expect(global.fetch).toHaveBeenCalled();
+      expect(global.fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/categories');
+      expect(categories).toEqual(successResponseBody);
+    });
+  });
+
+  it('getProductsFromCategory', () => {
+    const categoryId = 'category1';
+    const successResponseBody = {};
+
+    const mockFetchPromise = Promise.resolve({
+      json: () => Promise.resolve(successResponseBody)
+    });
+
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    return api.getProductsFromCategory(categoryId).then(products => {
+      expect(global.fetch).toHaveBeenCalled();
+      expect(global.fetch).toHaveBeenCalledWith(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+      expect(products).toEqual(successResponseBody);
+    });
+  });
+
+  it('getProductsFromQuery', () => {
+    const query = 'my-query';
+    const successResponseBody = {};
+
+    const mockFetchPromise = Promise.resolve({
+      json: () => Promise.resolve(successResponseBody)
+    });
+
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    return api.getProductsFromQuery(query).then(products => {
+      expect(global.fetch).toHaveBeenCalled();
+      expect(global.fetch).toHaveBeenCalledWith(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+      expect(products).toEqual(successResponseBody);
+    });
+  });
+
+  it('getProductsFromCategoryAndQuery', () => {
+    const categoryId = 'category1';
+    const query = 'my-query';
+    const successResponseBody = {};
+
+    const mockFetchPromise = Promise.resolve({
+      json: () => Promise.resolve(successResponseBody)
+    });
+
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    return api.getProductsFromCategoryAndQuery(categoryId, query).then(products => {
+      expect(global.fetch).toHaveBeenCalled();
+      expect(global.fetch).toHaveBeenCalledWith(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+      expect(products).toEqual(successResponseBody);
+    });
+  });
 });
