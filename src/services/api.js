31a1b50159a1645c97fc5960ed0eb542852cafed
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-04-30 15:02:56 -0300

    implement api functions to call mercado livre api

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+export async function getCategories() {
+  // implement here
+}
+
+export async function getProductsFromCategory(categoryId) {
+  // implement here
+}
+
+export async function getProductsFromQuery(query) {
+  // implement here
+}
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  // implement here
+}
